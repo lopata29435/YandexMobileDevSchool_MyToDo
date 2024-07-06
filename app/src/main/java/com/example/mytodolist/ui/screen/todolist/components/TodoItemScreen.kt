2424800b98a4abd855385eeab9713322731ca52c
lopata29435 <lopata29435@gmail.com>
2024-07-06 04:36:57 +0300

    Final

--- app/src/main/java/com/example/mytodolist/ui/screen/todolist/components/TodoItemScreen.kt
+++ app/src/main/java/com/example/mytodolist/ui/screen/todolist/components/TodoItemScreen.kt
@@ -2,9 +2,11 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
@@ -25,6 +27,8 @@ import com.example.mytodolist.Classes.TodoItem
 import com.example.mytodolist.R
 import com.example.mytodolist.ui.theme.LocalColors
 import com.example.mytodolist.ui.theme.MyTypography
+import java.text.DateFormat
+import java.util.Date
 
 @Composable
 fun TodoItemElement(
@@ -34,51 +38,70 @@ fun TodoItemElement(
     onCheckedChange: (Boolean) -> Unit
 ) {
     val colors = LocalColors.current
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .clickable(onClick = onClick)
             .background(
-                if(isSelected) colors.backSecondary
-                else colors.backSecondary)
-    )
-    {
-        Checkbox(
-            checked = todoItem.isCompleted,
-            onCheckedChange = { isChecked -> onCheckedChange(isChecked) }
-        )
-        Spacer(modifier = Modifier.width(4.dp))
-        if (todoItem.priority != Priority.DEFAULT) {
-            val painter = when(todoItem.priority) {
-                Priority.HIGH -> painterResource(id = R.drawable.high_priority)
-                Priority.LOW -> painterResource(id = R.drawable.low_priority)
-                else -> null
-            }
-            painter?.let {
-                Image(
-                    painter = it,
-                    contentDescription = "", // TODO повставлять описания
-                    modifier = Modifier
-                        .width(10.dp)
-                        .height(16.dp),
-                )
-            }
-        }
-        Spacer(modifier = Modifier.width(8.dp))
-        TodoItemText(
-            text = todoItem.text,
-            isCompleted = todoItem.isCompleted,
-            modifier = Modifier.weight(1f)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.info_outline),
-            contentDescription = "", // TODO повставлять описания
+                if (isSelected) colors.backSecondary
+                else colors.backSecondary
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .width(20.dp)
-                .height(20.dp)
+                .clickable(onClick = onClick)
+                .background(
+                    if (isSelected) colors.backSecondary
+                    else colors.backSecondary
+                )
         )
-        Spacer(modifier = Modifier.width(12.dp))
+        {
+            Checkbox(
+                checked = todoItem.isCompleted,
+                onCheckedChange = { isChecked -> onCheckedChange(isChecked) }
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            if (todoItem.priority != Priority.DEFAULT) {
+                val painter = when(todoItem.priority) {
+                    Priority.HIGH -> painterResource(id = R.drawable.high_priority)
+                    Priority.LOW -> painterResource(id = R.drawable.low_priority)
+                    else -> null
+                }
+                painter?.let {
+                    Image(
+                        painter = it,
+                        contentDescription = "", // TODO повставлять описания
+                        modifier = Modifier
+                            .width(10.dp)
+                            .height(16.dp),
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            TodoItemText(
+                text = todoItem.text,
+                isCompleted = todoItem.isCompleted,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.info_outline),
+                contentDescription = "", // TODO повставлять описания
+                modifier = Modifier
+                    .width(20.dp)
+                    .height(20.dp)
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+        }
+        todoItem.deadline?.let { deadline ->
+            val formattedDeadline = DateFormat.getDateInstance().format(Date(deadline.toLong()))
+            Text(
+                text = formattedDeadline,
+                color = colors.colorBlue,
+                modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
+            )
+        }
     }
 }
 
